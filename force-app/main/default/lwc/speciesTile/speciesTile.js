38a9aec0228a5d67c0c1a03ccfcfbf5057d17aa8
Pedro RM <pedro.reyes0284@alumnos.udg.mx>
2022-10-19 17:49:49 -0500

    LWC Species Explorer I-Base Layout

---
+++ force-app/main/default/lwc/speciesTile/speciesTile.js
@@ -0,0 +1,10 @@
+import { LightningElement } from 'lwc';
+
+export default class SpeciesTile extends LightningElement {
+    specie = {
+        Name: "Jazmin",
+        Description : "Olorosa y bonita planta trepadora",
+        Image_URL__c : "https://www.hogarmania.com/archivos/201911/jazmin-planta-medicinal-sedante-antidepresiva-1-XxXx80-1.jpg",
+        Location__c : "Indoors, Outdoors"
+    };
+}
